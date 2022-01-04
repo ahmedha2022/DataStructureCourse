e4a7684b338d80717c9cb100b1478544899bb5be
hato99 <72713637+hato99@users.noreply.github.com>
2022-01-04 04:38:33 +0300

    the main project

---
+++ Project_DS_Lap_V0/src/project/Student.java
@@ -0,0 +1,48 @@
+package project;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String name;
+	private double gpa;
+
+	public Student() {
+	}
+
+	public Student(int id, String name, double gpa) {
+		this.id = id;
+		this.name = name;
+		this.gpa = gpa;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getGpa() {
+		return gpa;
+	}
+
+	public void setGpa(double gpa) {
+		this.gpa = gpa;
+	}
+
+	public String toString() {
+		return String.format("%-10d | %-15s | %-20s \n", id, name, gpa);// format fixed
+	}
+
+}
